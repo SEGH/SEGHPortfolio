a14c2bfc5a43c8a05b25e4a7519160eca384fa80
Susan Holland <segh@fastmail.com>
2020-08-07 17:09:08 -0400

    Added Contact page component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <main className="container-md">
                 <Route exact path="/" component={About} />
                 <Route exact path="/portfolio" component={Portfolio} />
+                <Route exact path="/contact" component={Contact} />
             </main>
         </Router>
     );
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <section id="contact" className="jumbotron bg-light shadow-lg rounded mt-5 p-4">
+
+            <h2 className="display-5">Contact</h2>
+            <hr className="my-4" />
+
+        </section>
+    );
+}
