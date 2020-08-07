f1a8001462147e0d3eda9e881993e249984a7547
Susan Holland <segh@fastmail.com>
2020-08-07 17:04:30 -0400

    Added Portfolio page component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar />
             <main className="container-md">
                 <Route exact path="/" component={About} />
+                <Route exact path="/portfolio" component={Portfolio} />
             </main>
         </Router>
     );
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Portfolio() {
+    return (
+        <section id="portfolio" className="jumbotron bg-light shadow-lg rounded mt-5 p-4">
+
+            <h2 className="display-5">Portfolio</h2>
+            <hr className="my-4" />
+            <div className="row">
+                <h3>Cards go here</h3>
+            </div>
+
+        </section>
+    );
+}
