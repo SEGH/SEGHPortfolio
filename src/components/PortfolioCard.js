6c426a64b451e2880f5b22246aac98e16b3d38e0
Susan Holland <segh@fastmail.com>
2020-08-07 18:30:28 -0400

    PortfolioCard component with props

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function PortfolioCard(props) {
     return (
         <div className="card col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
-            <img src={props.image} className="card-img rounded-top" alt="Burger App Screenshot" />
+            <img src={props.image} className="card-img rounded-top" alt={props.alt} />
             <section className="card-footer bg-warning rounded-bottom">
                 <h4 className="card-title text-center">{props.title}</h4>
                 <div className="d-flex justify-content-around">
