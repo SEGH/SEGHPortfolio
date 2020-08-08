4904de49892beacc0bc6893e809606896bbf51bd
Susan Holland <segh@fastmail.com>
2020-08-08 10:02:32 -0400

    Updated container to reactstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "reactstrap";
 import Header from "./components/Header";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
@@ -11,11 +12,11 @@ function App() {
     return (
         <Router>
             <Header />
-            <main className="container-md">
+            <Container fluid="md">
                 <Route exact path="/" component={About} />
                 <Route exact path="/portfolio" component={Portfolio} />
                 <Route exact path="/contact" component={Contact} />
-            </main>
+            </Container>
             <Footer />
         </Router>
     );
