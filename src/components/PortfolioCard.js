691b1e5c39416e25364cf3459ced6a99ee77f6a5
Susan Holland <segh@fastmail.com>
2020-08-08 10:01:17 -0400

    Updated PortfolioCard to reactstrap

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,16 +1,17 @@
 import React from "react";
+import { Card, CardFooter } from "reactstrap";
 
 export default function PortfolioCard(props) {
     return (
-        <div className="card col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
+        <Card className="col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
             <img src={props.image} className="card-img rounded-top" alt={props.alt} />
-            <section className="card-footer bg-warning rounded-bottom">
+            <CardFooter className="bg-warning rounded-bottom">
                 <h4 className="card-title text-center">{props.title}</h4>
                 <div className="d-flex justify-content-around">
                     <a href={props.deployedLink} target="_blank" className="btn bg-light text-warning rounded m-2">Deployed App</a>
                     <a href={props.repoLink} target="_blank" className="btn bg-light text-warning rounded m-2">Repository</a>
                 </div>
-            </section>
-        </div>
+            </CardFooter>
+        </Card>
     );
 }
