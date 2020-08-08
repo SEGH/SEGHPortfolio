94c220bafd24af07e38a6ceb234b487042f0d184
Susan Holland <segh@fastmail.com>
2020-08-08 10:02:11 -0400

    Updated Contact to reactstrap

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Jumbotron } from "reactstrap";
 
 export default function Contact() {
     return (
-        <section id="contact" className="jumbotron bg-light shadow-lg rounded mt-5 p-4">
+        <Jumbotron id="contact" className="bg-light shadow-lg rounded mt-5 p-4">
 
             <h2 className="display-5">Contact</h2>
             <hr className="my-4" />
 
-        </section>
+        </Jumbotron>
     );
 }
