29ee10940b861a0f298afe76f4e53f8a2d9d81c0
Susan Holland <segh@fastmail.com>
2020-08-08 09:42:34 -0400

    Updated About to use reactstrap

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,37 +1,39 @@
 import React from "react";
 import image from "../../assets/bioimage.jpeg";
+import { Jumbotron, Card, CardText, CardBody, Row, Col } from "reactstrap";
 
 export default function About() {
     return (
-        <section id="main-bio" className="jumbotron bg-light shadow-lg rounded mt-5 p-4">
+        <Jumbotron id="main-bio" className="bg-light shadow-lg rounded mt-5 p-4">
 
             <h2 className="display-5">About Me</h2>
             <hr className="my-4" />
-            <div className="card border-0">
 
-                <div className="row no-gutters bg-light">
+            <Card className="border-0">
 
-                    <div className="col-md-4">
+                <Row className="no-gutters bg-light">
+
+                    <Col md="4">
                         <img src={image} class="card-img pb-4 rounded-0"
                             alt="Susan Holland profile" />
-                    </div>
+                    </Col>
 
-                    <div className="col-md-8">
-                        <div className="card-body p-0 px-md-4 mt-n1">
-                            <p className="card-text">
+                    <Col md="8">
+                        <CardBody className="p-0 px-md-4 mt-n1">
+                            <CardText>
                                 Now a web developer based in Philadelphia, I previously worked for fifteen years in the
                                 coffee industry, most recently for a small local coffee roaster. I first started my
                                 coding journey as a hobby when I came across the game Code Combat and was hooked. I also
                                 enjoy other hobbies that help to strengthen my problem-solving muscles, including
                                 sewing, home DIY projects, and playing piano.
-                        </p>
-                        </div>
-                    </div>
+                            </CardText>
+                        </CardBody>
+                    </Col>
 
-                </div>
+                </Row>
 
-            </div>
+            </Card>
 
-        </section>
+        </Jumbotron>
     );
 }
