f0975b18a4c9c3c326af965386dbb4b5c9015383
Susan Holland <segh@fastmail.com>
2020-09-02 15:45:30 -0400

    Added Return Safe and Book Search Portfolio Cards

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,6 +7,8 @@ import notes from "../../assets/768-notes.png";
 import plasma from "../../assets/768-plasma.png";
 import weather from "../../assets/768-weather.png";
 import burger from "../../assets/768alldone.png";
+import returnSafe from "../../assets/returnSafe.png";
+import searchBooks from "../../assets/searchBooks.png";
 
 export default function Portfolio() {
     return (
@@ -15,12 +17,14 @@ export default function Portfolio() {
             <h2 className="display-5">Portfolio</h2>
             <hr className="my-4" />
             <Row>
-                <PortfolioCard image={plasma} alt={"PLASMA project"} title={"PLASMA Dashboard"} deployedLink={"https://plasma-playlistdashboard.herokuapp.com/"} repoLink={"https://github.com/SEGH/PLASMA"}/>
-                <PortfolioCard image={burger} alt={"BurgerApp project"} title={"Burger App"} deployedLink={"https://enthousiaste-choucroute-00740.herokuapp.com/"} repoLink={"https://github.com/SEGH/burger"}/>
-                <PortfolioCard image={notes} alt={"takeNotes project"} title={"Note Taker"} deployedLink={"https://takenotes-22639.herokuapp.com"} repoLink={"https://github.com/SEGH/takeNotes"}/>
-                <PortfolioCard image={dayScheduler} alt={"Scheduler project"} title={"Day Scheduler"} deployedLink={"https://segh.github.io/DayScheduler/"} repoLink={"https://github.com/SEGH/DayScheduler"}/>
-                <PortfolioCard image={weather} alt={"Weather dashboard"} title={"Weather Dashboard"} deployedLink={"https://segh.github.io/WeatherDashboard/"} repoLink={"https://github.com/SEGH/WeatherDashboard"}/>
-                <PortfolioCard image={bookNook} alt={"Book Nook"} title={"Book Nook"} deployedLink={"https://segh.github.io/Book-Nook/"} repoLink={"https://github.com/SEGH/Book-Nook"}/>
+                <PortfolioCard image={returnSafe} alt={"Return Safe"} title={"Return Safe"} deployedLink={"https://return-safe.herokuapp.com/"} repoLink={"https://github.com/SEGH/Return-Safe"} />
+                <PortfolioCard image={searchBooks} alt={"Book Search"} title={"Book Search"} deployedLink={"https://googlebooks-80891.herokuapp.com/"} repoLink={"https://github.com/SEGH/bookSearch"} />
+                <PortfolioCard image={plasma} alt={"PLASMA project"} title={"PLASMA Dashboard"} deployedLink={"https://plasma-playlistdashboard.herokuapp.com/"} repoLink={"https://github.com/SEGH/PLASMA"} />
+                <PortfolioCard image={burger} alt={"BurgerApp project"} title={"Burger App"} deployedLink={"https://enthousiaste-choucroute-00740.herokuapp.com/"} repoLink={"https://github.com/SEGH/burger"} />
+                <PortfolioCard image={notes} alt={"takeNotes project"} title={"Note Taker"} deployedLink={"https://takenotes-22639.herokuapp.com"} repoLink={"https://github.com/SEGH/takeNotes"} />
+                <PortfolioCard image={dayScheduler} alt={"Scheduler project"} title={"Day Scheduler"} deployedLink={"https://segh.github.io/DayScheduler/"} repoLink={"https://github.com/SEGH/DayScheduler"} />
+                <PortfolioCard image={weather} alt={"Weather dashboard"} title={"Weather Dashboard"} deployedLink={"https://segh.github.io/WeatherDashboard/"} repoLink={"https://github.com/SEGH/WeatherDashboard"} />
+                <PortfolioCard image={bookNook} alt={"Book Nook"} title={"Book Nook"} deployedLink={"https://segh.github.io/Book-Nook/"} repoLink={"https://github.com/SEGH/Book-Nook"} />
             </Row>
 
         </Jumbotron>
