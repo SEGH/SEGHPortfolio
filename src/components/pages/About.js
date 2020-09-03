4a96c86ea59eac4f7299dc772fa78d47957fcda9
Susan Holland <segh@fastmail.com>
2020-09-03 16:06:15 -0400

    Added skill icons

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,8 @@
 import React from "react";
 import image from "../../assets/bioimage.jpeg";
 import { Jumbotron, Card, CardText, CardBody, Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faGitAlt, faNode, faHtml5, faJsSquare, faCss3Alt, faBootstrap } from "@fortawesome/free-brands-svg-icons";
 
 export default function About() {
     return (
@@ -27,6 +29,43 @@ export default function About() {
                                 enjoy other hobbies that help to strengthen my problem-solving muscles, including
                                 sewing, home DIY projects, and playing piano.
                             </CardText>
+
+                            <Row id="skillIcons">
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faReact} spin size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faGitAlt} size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faNode} size="3x" />
+                                </Col>
+                                <Col  xs="4" md="3">
+                                    <FontAwesomeIcon icon={faHtml5} size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faJsSquare} size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faCss3Alt} size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <FontAwesomeIcon icon={faBootstrap} size="3x" />
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <i className="fab icon-mysql" style={{ fontSize: "3em", position: "relative", top: "-10px"}}></i>
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <i className="fab icon-mongodb" style={{ fontSize: "3em", position: "relative", top: "-10px"}}></i>
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <i className="fab icon-heroku" style={{ fontSize: "3em", position: "relative", top: "-10px"}}></i>
+                                </Col>
+                                <Col xs="4" md="3">
+                                    <i className="fab icon-jquery" style={{ fontSize: "3em", position: "relative", top: "-10px"}}></i>
+                                </Col>
+                            </Row>
+
                         </CardBody>
                     </Col>
 
