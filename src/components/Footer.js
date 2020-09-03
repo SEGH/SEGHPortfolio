830982d74d0320ff4715bee179f0a45967369213
Susan Holland <segh@fastmail.com>
2020-09-03 16:05:28 -0400

    Converted i tags to FontAwesome components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return (
@@ -9,20 +12,20 @@ export default function Footer() {
 
                 <NavItem>
                     <NavLink className="text-white" href="mailto:segh@fastmail.com" target="_blank">
-                        <i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+                        <FontAwesomeIcon icon={faEnvelope} size="2x" />
                     </NavLink>
                 </NavItem>
 
                 <NavItem>
                     <NavLink className="text-white" href="https://github.com/SEGH" target="_blank">
-                        <i class="fa fa-github fa-2x" aria-hidden="true"></i>
+                        <FontAwesomeIcon icon={faGithub} size="2x" />
                     </NavLink>
                 </NavItem>
 
                 <NavItem>
                     <NavLink className="text-white" href="https://www.linkedin.com/in/susan-ella-grace-holland/"
                         target="_blank">
-                        <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+                        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
                     </NavLink>
                 </NavItem>
 
