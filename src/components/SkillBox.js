06962e2e01ba45b4735bce1582312c8c5f8ad69f
Susan Holland <segh@fastmail.com>
2020-09-04 14:11:33 -0400

    Adjusted columns for large breakpoints

--- src/components/SkillBox.js
+++ src/components/SkillBox.js
@@ -8,64 +8,64 @@ export default function SkillBox() {
     return (
         <Row id="skillIcons">
 
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg={{ size: 2, offset: 1 }}>
                 <FontAwesomeIcon icon={faNode} size="3x" />
                 <p>Node.js</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="devicon-express-original" style={{ fontSize: "3em" }}></i>
                 <p>Express</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="devicon-handlebars-plain" style={{ fontSize: "3em" }}></i>
                 <p>Handlebars</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="devicon-sequelize-plain" style={{ fontSize: "3em" }}></i>
                 <p>Sequelize</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="fab icon-mysql" style={{ fontSize: "3em", position: "relative", top: "-10px" }}></i>
                 <p style={{ marginTop: "-10px" }}>MySQL</p>
             </Col>
 
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg={{ size: 2, offset: 1 }}>
                 <i className="fab icon-mongodb" style={{ fontSize: "3em", position: "relative", top: "-10px" }}></i>
                 <p style={{ marginTop: "-10px" }}>MongoDB</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="fab icon-heroku" style={{ fontSize: "3em", position: "relative", top: "-10px" }}></i>
                 <p style={{ marginTop: "-10px" }}>Heroku</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <FontAwesomeIcon icon={faGitAlt} size="3x" />
                 <p>Git</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <FontAwesomeIcon icon={faHtml5} size="3x" />
                 <p>HTML 5</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <FontAwesomeIcon icon={faCss3Alt} size="3x" />
                 <p>CSS 3</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg={{ size: 2, offset: 1 }}>
                 <FontAwesomeIcon icon={faBootstrap} size="3x" />
                 <p>Bootstrap</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <DevIcon icon="materializecss" width="3.5em" style={{ fill: "#444" }} />
                 <p>Materialize</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <FontAwesomeIcon icon={faJsSquare} size="3x" />
                 <p>JavaScript</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <i className="fab icon-jquery" style={{ fontSize: "3em", position: "relative", top: "-10px" }}></i>
                 <p style={{ marginTop: "-10px" }}>jQuery</p>
             </Col>
-            <Col xs="4" md="3">
+            <Col xs="4" md="3" lg="2">
                 <FontAwesomeIcon icon={faReact} spin size="3x" />
                 <p>React</p>
             </Col>
