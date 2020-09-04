728dee8cceac000e3148f3117726dbac553180d3
Susan Holland <segh@fastmail.com>
2020-09-04 13:03:25 -0400

    Imported devicon.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootswatch/dist/pulse/bootstrap.min.css";
 import "../node_modules/font-mfizz/dist/font-mfizz.css";
+import "../node_modules/devicon/devicon.css";
 import "./assets/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
