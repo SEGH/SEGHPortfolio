025003918d90feb6b6c610b725d02d3ce1e8006e
Susan Holland <segh@fastmail.com>
2020-09-07 11:40:35 -0400

    Changed card footer color palette and title to small caps

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -8,7 +8,7 @@ export default function PortfolioCard(props) {
     return (
         <Card className="col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
             <img src={props.image} className="card-img rounded-top" alt={props.alt} />
-            <CardFooter className="bg-warning rounded-bottom">
+            <CardFooter className="rounded-bottom">
                 <h4 className="card-title text-center">{props.title}</h4>
                 <div className="d-flex justify-content-around">
                     <a href={props.deployedLink} target="_blank" className="btn"><FontAwesomeIcon icon={faExternalLinkAlt} size="3x" /></a>
