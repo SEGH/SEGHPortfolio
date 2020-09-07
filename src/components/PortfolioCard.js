652f472875de9ec3487d6f14330f07a83e33dc36
Susan Holland <segh@fastmail.com>
2020-09-07 13:57:10 -0400

    Changed footer order

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -9,12 +9,12 @@ export default function PortfolioCard(props) {
         <Card className="col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
             <img src={props.image} className="card-img rounded-top" alt={props.alt} />
             <CardFooter className="rounded-bottom">
+                <div className="d-flex justify-content-around">
+                    <a href={props.deployedLink} target="_blank" title="Deployed App" className="btn"><FontAwesomeIcon icon={faExternalLinkAlt} size="2x" /></a>
+                    <a href={props.repoLink} target="_blank" title="GitHub Repo" className="btn"><FontAwesomeIcon icon={faGithubSquare} size="2x" /></a>
+                </div>
                 <h4 className="card-title text-center">{props.title}</h4>
                 <p>{props.description}</p>
-                <div className="d-flex justify-content-around">
-                    <a href={props.deployedLink} target="_blank" title="Deployed App" className="btn"><FontAwesomeIcon icon={faExternalLinkAlt} size="3x" /></a>
-                    <a href={props.repoLink} target="_blank" title="GitHub Repo" className="btn"><FontAwesomeIcon icon={faGithubSquare} size="3x" /></a>
-                </div>
             </CardFooter>
         </Card>
     );
