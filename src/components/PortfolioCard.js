a51877c206b3552ab08fd87893dd66713e949f1d
Susan Holland <segh@fastmail.com>
2020-09-08 14:40:03 -0400

    Changed btn alignment and github icon

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardFooter } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare, faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function PortfolioCard(props) {
@@ -9,9 +9,9 @@ export default function PortfolioCard(props) {
         <Card className="col-sm-6 col-lg-4 bg-light border-0 text-light py-3">
             <img src={props.image} className="card-img rounded-top" alt={props.alt} />
             <CardFooter className="rounded-bottom">
-                <div className="d-flex justify-content-around">
+                <div className="d-flex justify-content-between pb-1">
                     <a href={props.deployedLink} target="_blank" rel="noopener noreferrer" title="Deployed App" className="btn"><FontAwesomeIcon icon={faExternalLinkAlt} size="2x" /></a>
-                    <a href={props.repoLink} target="_blank" rel="noopener noreferrer" title="GitHub Repo" className="btn"><FontAwesomeIcon icon={faGithubSquare} size="2x" /></a>
+                    <a href={props.repoLink} target="_blank" rel="noopener noreferrer" title="GitHub Repo" className="btn"><FontAwesomeIcon icon={faGithubAlt} size="2x" /></a>
                 </div>
                 <h4 className="card-title text-center">{props.title}</h4>
                 <p>{props.description}</p>
