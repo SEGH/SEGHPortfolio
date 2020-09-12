110e891628812e3c3ab28aa74baf5434db3fb7ef
Susan Holland <segh@fastmail.com>
2020-09-12 16:23:19 -0400

    Added div around Navbar to fix flexbox issue in safari

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,31 +6,33 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return (
-        <Navbar className="mt-auto justify-content-center" dark color="dark">
+        <div>
+            <Navbar className="mt-auto justify-content-center" dark color="dark">
 
-            <Nav className="nav justify-content-center">
+                <Nav className="nav justify-content-center">
 
-                <NavItem>
-                    <NavLink className="text-white" href="mailto:segh@fastmail.com" target="_blank">
-                        <FontAwesomeIcon icon={faEnvelope} size="2x" />
-                    </NavLink>
-                </NavItem>
+                    <NavItem>
+                        <NavLink className="text-white" href="mailto:segh@fastmail.com" target="_blank">
+                            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                        </NavLink>
+                    </NavItem>
 
-                <NavItem>
-                    <NavLink className="text-white" href="https://github.com/SEGH" target="_blank">
-                        <FontAwesomeIcon icon={faGithub} size="2x" />
-                    </NavLink>
-                </NavItem>
+                    <NavItem>
+                        <NavLink className="text-white" href="https://github.com/SEGH" target="_blank">
+                            <FontAwesomeIcon icon={faGithub} size="2x" />
+                        </NavLink>
+                    </NavItem>
 
-                <NavItem>
-                    <NavLink className="text-white" href="https://www.linkedin.com/in/susan-ella-grace-holland/"
-                        target="_blank">
-                        <FontAwesomeIcon icon={faLinkedin} size="2x"/>
-                    </NavLink>
-                </NavItem>
+                    <NavItem>
+                        <NavLink className="text-white" href="https://www.linkedin.com/in/susan-ella-grace-holland/"
+                            target="_blank">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        </NavLink>
+                    </NavItem>
 
-            </Nav>
+                </Nav>
 
-        </Navbar>
+            </Navbar>
+        </div>
     );
 }
