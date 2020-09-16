21ca2f23f8b5f1e581a3b59a29506bd5e4a4e162
Susan Holland <segh@fastmail.com>
2020-09-16 14:32:07 -0400

    Added unique keys to tool icons

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -20,33 +20,33 @@ export default function PortfolioCard(props) {
                     {props.tools ? props.tools.map(tool => {
                         switch (tool) {
                             case "node":
-                                return <FontAwesomeIcon icon={faNode} size="2x" title="Node.JS" />
+                                return <FontAwesomeIcon icon={faNode} size="2x" title="Node.JS" key={props.title + tool} />
                             case "html":
-                                return <FontAwesomeIcon icon={faHtml5} size="2x" title="HTML5" />
+                                return <FontAwesomeIcon icon={faHtml5} size="2x" title="HTML5" key={props.title + tool} />
                             case "css":
-                                return <FontAwesomeIcon icon={faCss3Alt} size="2x" title="CSS3" />
+                                return <FontAwesomeIcon icon={faCss3Alt} size="2x" title="CSS3" key={props.title + tool} />
                             case "javaScript":
-                                return <FontAwesomeIcon icon={faJsSquare} size="2x" title="JavaScript" />
+                                return <FontAwesomeIcon icon={faJsSquare} size="2x" title="JavaScript" key={props.title + tool} />
                             case "bootstrap":
-                                return <FontAwesomeIcon icon={faBootstrap} size="2x" title="Bootstrap" />
+                                return <FontAwesomeIcon icon={faBootstrap} size="2x" title="Bootstrap" key={props.title + tool} />
                             case "react":
-                                return <FontAwesomeIcon icon={faReact} size="2x" title="React" />
+                                return <FontAwesomeIcon icon={faReact} size="2x" title="React" key={props.title + tool} />
                             case "mongodb":
-                                return <i className="fab icon-mongodb" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="MongoDB"></i>
+                                return <i className="fab icon-mongodb" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="MongoDB" key={props.title + tool}></i>
                             case "express":
-                                return <i className="devicon-express-original" style={{ fontSize: "2em" }} title="Express"></i>
+                                return <i className="devicon-express-original" style={{ fontSize: "2em" }} title="Express" key={props.title + tool}></i>
                             case "handlebars":
-                                return <i className="devicon-handlebars-plain" style={{ fontSize: "2em" }} title="Handlebars"></i>
+                                return <i className="devicon-handlebars-plain" style={{ fontSize: "2em" }} title="Handlebars" key={props.title + tool}></i>
                             case "sequelize":
-                                return <i className="devicon-sequelize-plain" style={{ fontSize: "2em" }} title="Sequelize"></i>
+                                return <i className="devicon-sequelize-plain" style={{ fontSize: "2em" }} title="Sequelize" key={props.title + tool}></i>
                             case "mySql":
-                                return <i className="fab icon-mysql" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="MySQL"></i>
+                                return <i className="fab icon-mysql" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="MySQL" key={props.title + tool}></i>
                             case "materialize":
-                                return <DiMaterializecss style={{ fontSize: "2.5em"}} title="Materialize" />
+                                return <DiMaterializecss style={{ fontSize: "2.5em"}} title="Materialize" key={props.title + tool} />
                             case "jquery":
-                                return <i className="fab icon-jquery" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="jQuery"></i>
+                                return <i className="fab icon-jquery" style={{ fontSize: "2em", position: "relative", top: "-7px" }} title="jQuery" key={props.title + tool}></i>
                             default:
-                                return <FontAwesomeIcon icon={faCode} style={{ fontSize: "1.5em", position:"relative", top: "3px" }} title={tool} />;
+                                return <FontAwesomeIcon icon={faCode} style={{ fontSize: "1.5em", position:"relative", top: "3px" }} title={tool} key={props.title + tool} />;
                         }
                     }) : <FontAwesomeIcon icon={faGithubAlt} size="2x" />}
                 </div>
