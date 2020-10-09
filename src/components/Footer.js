a0b36b2c85ec4b1719dee03ba6baf2477ec8d644
Susan Holland <segh@fastmail.com>
2020-10-09 16:42:02 -0400

    Added descriptionp tags in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,28 +6,31 @@ import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return (
-        <div className="mt-auto">
-            <Navbar className="justify-content-center" dark color="dark">
+        <div id="footer" className="mt-auto">
+            <Navbar className="justify-content-center pt-3 pb-3" dark color="dark">
 
                 <Nav className="nav justify-content-center">
 
-                    <NavItem>
-                        <NavLink className="text-white" href="mailto:SEG.Holland@gmail.com" target="_blank">
+                    <NavItem className="text-center">
+                        <NavLink className="text-white pb-0" href="mailto:SEG.Holland@gmail.com" target="_blank">
                             <FontAwesomeIcon icon={faEnvelope} size="2x" />
                         </NavLink>
+                        <p className="mb-0 ml-n4">SEG.Holland@gmail.com</p>
                     </NavItem>
 
-                    <NavItem>
-                        <NavLink className="text-white" href="https://github.com/SEGH" target="_blank">
+                    <NavItem className="text-center">
+                        <NavLink className="text-white pb-0" href="https://github.com/SEGH" target="_blank">
                             <FontAwesomeIcon icon={faGithub} size="2x" />
                         </NavLink>
+                        <p className="mb-0">GitHub</p>
                     </NavItem>
 
-                    <NavItem>
-                        <NavLink className="text-white" href="https://www.linkedin.com/in/susan-ella-grace-holland/"
+                    <NavItem className="text-center">
+                        <NavLink className="text-white pb-0" href="https://www.linkedin.com/in/susan-ella-grace-holland/"
                             target="_blank">
                             <FontAwesomeIcon icon={faLinkedin} size="2x" />
                         </NavLink>
+                        <p className="mb-0">LinkedIn</p>
                     </NavItem>
 
                 </Nav>
