fca714d74fef570ff3fe66d829b93269432e9aa6
Susan Holland <segh@fastmail.com>
2020-10-09 16:58:25 -0400

    Added tooltip to email icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
-import React from "react";
-import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, NavItem, NavLink, Tooltip } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+
+    const toggle = () => setTooltipOpen(!tooltipOpen);
+
     return (
         <div id="footer" className="mt-auto">
             <Navbar className="justify-content-center pt-3 pb-3" dark color="dark">
@@ -12,7 +16,10 @@ export default function Footer() {
                 <Nav className="nav justify-content-center">
 
                     <NavItem className="text-center">
-                        <NavLink className="text-white pb-0" href="mailto:SEG.Holland@gmail.com" target="_blank">
+                        <Tooltip placement="top" isOpen={tooltipOpen} target="mailIcon" toggle={toggle}>
+                            Right click to copy email!
+                        </Tooltip>
+                        <NavLink id="mailIcon" className="text-white pb-0" href="mailto:SEG.Holland@gmail.com" target="_blank">
                             <FontAwesomeIcon icon={faEnvelope} size="2x" />
                         </NavLink>
                         <p className="mb-0 ml-n4">SEG.Holland@gmail.com</p>
