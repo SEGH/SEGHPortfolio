df9143e92b4643036e01b4830b19c8c8cb868e2a
Susan Holland <segh@fastmail.com>
2021-02-12 13:07:19 -0500

    Lazy load webp greenLight background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "reactstrap";
 import Header from "./components/Header";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Footer from "./components/Footer";
+import { greenLight as svgBackground } from "./assets/greenLight.svg";
+import greenLight from "./assets/greenLight.webp";
 
 function App() {
+    
+    useEffect(() => {
+        const imageLoader = new Image();
+        imageLoader.src = greenLight;
+
+        imageLoader.onload = () => {
+            console.log("source")
+            document.body.style.backgroundImage = `url(${imageLoader.src})`;
+        }
+    }, []);
+    
     return (
         <Router>
             <Header />
