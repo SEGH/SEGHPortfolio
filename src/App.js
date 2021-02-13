ad57ca6d1ee8f4c16b41a08db6129de97ebf2f28
Susan Holland <segh@fastmail.com>
2021-02-12 19:00:35 -0500

    Add useEffect to set greenLight webp as background when loaded

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "reactstrap";
 import Header from "./components/Header";
 import About from "./components/pages/About";
 import Portfolio from "./components/pages/Portfolio";
 import Footer from "./components/Footer";
-import { greenLight as svgBackground } from "./assets/greenLight.svg";
 import greenLight from "./assets/greenLight.webp";
 
 function App() {
-    
-    useEffect(() => {
-        const imageLoader = new Image();
-        imageLoader.src = greenLight;
 
-        imageLoader.onload = () => {
-            console.log("source")
-            document.body.style.backgroundImage = `url(${imageLoader.src})`;
+    useEffect(() => {
+        const background = new Image();
+        background.src = greenLight;
+
+        background.onload = () => {
+            document.body.style.backgroundImage = `url(${background.src})`;
         }
     }, []);
     
